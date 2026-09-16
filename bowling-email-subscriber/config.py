5e2e3ba8c3e447901a41f6aa1544719138fc6ea2
slidwell44 <slidwell@gmail.com>
2026-09-15 23:53:26 -0400

    I'm Batman

---
+++ bowling-email-subscriber/config.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class GoogleSettings(BaseSettings): ...
+
+
+class Settings(BaseModel): ...
---
+++ bowling-email-subscriber/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from bowling-email-subscriber!")
+
+
+if __name__ == "__main__":
+    main()
